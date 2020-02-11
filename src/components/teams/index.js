0f7fff5983285340678834bb2e7f4bfafcbf6bd3
Kitiya <kate.kitiya@gmail.com>
2020-02-11 17:09:07 -0500

    Add a search team functionality

--- src/components/teams/index.js
+++ src/components/teams/index.js
@@ -7,7 +7,7 @@ import { URL_TEAMS } from "../utils/paths";
 const Teams = () => {
   const [teams, setTeams] = useState([]);
   const [filtered, setFiltered] = useState([]);
-  const [keyword, setKeyword] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
     const fetchTeam = () => {
@@ -29,10 +29,37 @@ const Teams = () => {
         </Link>
       </CSSTransition>
     ));
+
+  useEffect(() => {
+    const searchTeams = () => {
+      if (keyword !== "") {
+        const list = teams.filter(team => {
+          return team.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1;
+        });
+
+        setFiltered(list);
+      } else {
+        setFiltered(teams);
+        setKeyword("");
+      }
+    };
+
+    searchTeams();
+  }, [teams, keyword]);
+
+  const handleSearchChange = e => {
+    setKeyword(e.target.value);
+  };
+
   return (
     <div className="teams_component">
       <div className="teams_input">
-        <input type="text" value={keyword} placeholder="Search for a team" />
+        <input
+          type="text"
+          value={keyword}
+          placeholder="Search for a team"
+          onChange={e => handleSearchChange(e)}
+        />
       </div>
       <div className="teams_container">
         <TransitionGroup component="span">{renderTeams()}</TransitionGroup>
